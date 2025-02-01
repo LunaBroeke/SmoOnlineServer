f6463d8dd8ace63a10c6869f806d4ae7f5917cbc
LunaBroeke <onefivefoursakura@gmail.com>
2025-02-01 14:26:51 +0100

    Brought some stuff here from main

--- Server/HnSRandom.cs
+++ Server/HnSRandom.cs
@@ -10,11 +10,23 @@ namespace Server
 	public class HnSRandom
 	{
 		public static Server server;
-		public static readonly string[] stageNames = { "CapWorldHomeStage","WaterfallWorldHomeStage","SandWorldHomeStage","LakeWorldHomeStage","ForestWorldHomeStage","ClashWorldHomeStage","CityWorldHomeStage","SnowWorldHomeStage","SeaWorldHomeStage","LavaWorldHomeStage","SkyWorldHomeStage","MoonWorldHomeStage","PeachWorldHomeStage"};
-
-		public static string StartRandomGame()
+		public static string StartRandomGame(string arg)
 		{
 			string stage = RandomStage();
+			int num = 0;
+			try { num = int.Parse(arg); }
+			catch { return "Invalid number"; }
+			if (num > server.Clients.Count)
+				return "Number is higher than the amount of players";
+			else if (num == server.Clients.Count)
+				return "Please have at least one hider available";
+			List<string> players = new List<string>();
+			for (int i = 0; i < num; i++)
+			{
+				string n = RandomPlayer();
+				if (players.Contains(n)) { i--; continue; }
+				players.Add(n);
+			}
 			foreach (Client client in server.Clients)
 			{
 
@@ -24,17 +36,24 @@ namespace Server
 					Stage = stage,
 					Scenario = -1,
 				});
-#pragma warning restore CS4014 
+#pragma warning restore CS4014
 			}
-			// Still needs logic for selecting a random seeker(s) (amount would be selectable by doing 'randomgame [int]') and then displaying the seekers names. I haven't seen a way to broadcast messages so my current concept is to have a fake player display the seekers names.
-			// Remember to add a debug command to: Save Player Locations, Test the Fake Player.
-			return $"Starting Random Game on {stage}";
+			// To-do: Fake Player System to display messages, debug command to: Save Player Locations, Test the Fake Player.
+			string play = string.Join(',', players.ToArray());
+			return $"Starting Random Game on {stage} with seeker(s): {play}";
 		}
 
-		public static string RandomStage()
+		private static string RandomStage()
 		{
 			Random r = new Random();
-			int i = r.Next(0,stageNames.Length);
+
+			List<string> stageNames = new List<string>();
+			foreach (var stage in Settings.Instance.StageSelection)
+			{
+				if (stage.enabled == true)
+					stageNames.Add(stage.stageName);
+			}
+			int i = r.Next(0, stageNames.Count);
 			try
 			{
 				return stageNames[i];
@@ -43,6 +62,20 @@ namespace Server
 			{
 				server.Logger.Warn("Please tell Luwuna that she doesnt know how Random works and that the array overflowed");
 				return stageNames[i - 1];//I honestly dont remember how random works so I just have this incase it does go out of bounds
+			}
+		}
+
+		private static string RandomPlayer()
+		{
+			Random r = new Random();
+			int i = r.Next(0, server.Clients.Count);
+			try
+			{
+				return server.Clients[i].Name;
+			}
+			catch
+			{
+				return server.Clients[i - 1].Name;
 			}
 		}
 	}
--- Server/Settings.cs
+++ Server/Settings.cs
@@ -6,94 +6,151 @@ using Shared;
 
 namespace Server;
 
-public class Settings {
-    public static Settings Instance = new Settings();
-    private static readonly Logger Logger = new Logger("Settings");
-    public static Action? LoadHandler;
+public class Settings
+{
+	public static Settings Instance = new Settings();
+	private static readonly Logger Logger = new Logger("Settings");
+	public static Action? LoadHandler;
 
-    static Settings() {
-        LoadSettings();
-    }
+	static Settings()
+	{
+		LoadSettings();
+	}
 
-    public static void LoadSettings() {
-        if (File.Exists("settings.json")) {
-            string text = File.ReadAllText("settings.json");
-            try {
-                Instance = JsonConvert.DeserializeObject<Settings>(text, new StringEnumConverter(new CamelCaseNamingStrategy())) ?? Instance;
-                Logger.Info("Loaded settings from settings.json");
-            }
-            catch (Exception e) {
-                Logger.Warn($"Failed to load settings.json: {e}");
-            }
-        }
-        SaveSettings();
-        LoadHandler?.Invoke();
-    }
+	public static void LoadSettings()
+	{
+		if (File.Exists("settings.json"))
+		{
+			string text = File.ReadAllText("settings.json");
+			try
+			{
+				Instance = JsonConvert.DeserializeObject<Settings>(text, new StringEnumConverter(new CamelCaseNamingStrategy())) ?? Instance;
+				Logger.Info("Loaded settings from settings.json");
+				if (Instance.StageSelection == null || Instance.StageSelection.Count < 1)
+					Instance.StageSelection = StageSelectionTable.Preset();
+			}
+			catch (Exception e)
+			{
+				Logger.Warn($"Failed to load settings.json: {e}");
+			}
+		}
+		SaveSettings();
+		LoadHandler?.Invoke();
+	}
 
-    public static void SaveSettings(bool silent = false) {
-        try {
-            File.WriteAllText("settings.json", JsonConvert.SerializeObject(Instance, Formatting.Indented, new StringEnumConverter(new CamelCaseNamingStrategy())));
-            if (!silent) { Logger.Info("Saved settings to settings.json"); }
-        }
-        catch (Exception e) {
-            Logger.Error($"Failed to save settings.json {e}");
-        }
-    }
+	public static void SaveSettings(bool silent = false)
+	{
+		try
+		{
+			File.WriteAllText("settings.json", JsonConvert.SerializeObject(Instance, Formatting.Indented, new StringEnumConverter(new CamelCaseNamingStrategy())));
+			if (!silent) { Logger.Info("Saved settings to settings.json"); }
+		}
+		catch (Exception e)
+		{
+			Logger.Error($"Failed to save settings.json {e}");
+		}
+	}
 
-    public ServerTable Server { get; set; } = new ServerTable();
-    public FlipTable Flip { get; set; } = new FlipTable();
-    public ScenarioTable Scenario { get; set; } = new ScenarioTable();
-    public BanListTable BanList { get; set; } = new BanListTable();
-    public DiscordTable Discord { get; set; } = new DiscordTable();
-    public ShineTable Shines { get; set; } = new ShineTable();
-    public PersistShinesTable PersistShines { get; set; } = new PersistShinesTable();
-    public JsonApiTable JsonApi { get; set; } = new JsonApiTable();
+	public ServerTable Server { get; set; } = new ServerTable();
+	public FlipTable Flip { get; set; } = new FlipTable();
+	public ScenarioTable Scenario { get; set; } = new ScenarioTable();
+	public BanListTable BanList { get; set; } = new BanListTable();
+	public List<StageSelectionTable> StageSelection { get; set; } = new List<StageSelectionTable>();
+	public DiscordTable Discord { get; set; } = new DiscordTable();
+	public ShineTable Shines { get; set; } = new ShineTable();
+	public PersistShinesTable PersistShines { get; set; } = new PersistShinesTable();
+	public BannedNameTable BannedName { get; set; } = new BannedNameTable();
+	public JsonApiTable JsonApi { get; set; } = new JsonApiTable();
+	public class ServerTable
+	{
+		public string Address { get; set; } = IPAddress.Any.ToString();
+		public ushort Port { get; set; } = 1027;
+		public ushort MaxPlayers { get; set; } = 8;
+	}
 
-    public class ServerTable {
-        public string Address { get; set; } = IPAddress.Any.ToString();
-        public ushort Port { get; set; } = 1027;
-        public ushort MaxPlayers { get; set; } = 8;
-    }
+	public class ScenarioTable
+	{
+		public bool MergeEnabled { get; set; } = false;
+	}
 
-    public class ScenarioTable {
-        public bool MergeEnabled { get; set; } = false;
-    }
+	public class BanListTable
+	{
+		public bool Enabled { get; set; } = false;
+		public ISet<Guid> Players { get; set; } = new SortedSet<Guid>();
+		public ISet<string> IpAddresses { get; set; } = new SortedSet<string>();
+		public ISet<string> Stages { get; set; } = new SortedSet<string>();
+	}
 
-    public class BanListTable {
-        public bool Enabled { get; set; } = false;
-        public ISet<Guid> Players { get; set; } = new SortedSet<Guid>();
-        public ISet<string> IpAddresses { get; set; } = new SortedSet<string>();
-        public ISet<string> Stages { get; set; } = new SortedSet<string>();
-    }
+	public class StageSelectionTable
+	{
+		public string stageName { get; set; }
+		public bool enabled { get; set; } = true;
+		public StageSelectionTable(string name)
+		{
+			stageName = name;
+		}
+		public static List<StageSelectionTable> Preset()
+		{
+			List<StageSelectionTable> list = new List<StageSelectionTable>
+			{
+				new StageSelectionTable("CapWorldHomeStage"),
+				new StageSelectionTable("WaterfallWorldHomeStage"),
+				new StageSelectionTable("SandWorldHomeStage"),
+				new StageSelectionTable("LakeWorldHomeStage"),
+				new StageSelectionTable("ForestWorldHomeStage"),
+				new StageSelectionTable("CloudWorldHomeStage") { enabled = false },
+				new StageSelectionTable("ClashWorldHomeStage"),
+				new StageSelectionTable("CityWorldHomeStage"),
+				new StageSelectionTable("SnowWorldHomeStage"),
+				new StageSelectionTable("SeaWorldHomeStage"),
+				new StageSelectionTable("LavaWorldHomeStage"),
+				new StageSelectionTable("BossRaidWorldHomeStage") { enabled = false },
+				new StageSelectionTable("SkyWorldHomeStage"),
+				new StageSelectionTable("MoonWorldHomeStage"),
+				new StageSelectionTable("PeachWorldHomeStage"),
+				new StageSelectionTable("Special1WorldHomeStage") { enabled = false },
+				new StageSelectionTable("Special2WorldHomeStage") { enabled = false },
+			};
+			return list;
+		}
+	}
 
-    public class FlipTable {
-        public bool Enabled { get; set; } = true;
-        public ISet<Guid> Players { get; set; } = new SortedSet<Guid>();
-        public FlipOptions Pov { get; set; } = FlipOptions.Both;
-    }
+	public class FlipTable
+	{
+		public bool Enabled { get; set; } = true;
+		public ISet<Guid> Players { get; set; } = new SortedSet<Guid>();
+		public FlipOptions Pov { get; set; } = FlipOptions.Both;
+	}
 
-    public class DiscordTable {
-        public string? Token { get; set; }
-        public string Prefix { get; set; } = "$";
-        public string? CommandChannel { get; set; }
-        public string? LogChannel { get; set; }
-    }
+	public class DiscordTable
+	{
+		public string? Token { get; set; }
+		public string Prefix { get; set; } = "$";
+		public string? CommandChannel { get; set; }
+		public string? LogChannel { get; set; }
+	}
 
-    public class ShineTable {
-        public bool Enabled { get; set; } = true;
-        public ISet<int> Excluded { get; set; } = new SortedSet<int> { 496 };
-        public bool ClearOnNewSaves { get; set; } = false;
-    }
+	public class ShineTable
+	{
+		public bool Enabled { get; set; } = true;
+		public ISet<int> Excluded { get; set; } = new SortedSet<int> { 496 };
+		public bool ClearOnNewSaves { get; set; } = false;
+	}
 
-    public class PersistShinesTable
-    {
-        public bool Enabled { get; set; } = false;
-        public string Filename { get; set; } = "./moons.json";
-    }
+	public class PersistShinesTable
+	{
+		public bool Enabled { get; set; } = false;
+		public string Filename { get; set; } = "./moons.json";
+	}
+	public class BannedNameTable
+	{
+		public string[] names = new string[] { "yuzu", "suyu", "ryuplayer" };
+		public string StageToSend = "Special2WorldLavaStage";
+	}
 
-    public class JsonApiTable
-    {
-        public bool Enabled { get; set; } = false;
-        public Dictionary<string, SortedSet<string>> Tokens { get; set; } = new Dictionary<string, SortedSet<string>>();
-    }
+	public class JsonApiTable
+	{
+		public bool Enabled { get; set; } = false;
+		public Dictionary<string, SortedSet<string>> Tokens { get; set; } = new Dictionary<string, SortedSet<string>>();
+	}
 }
